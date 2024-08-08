f4c53b7c15b53f13a1ff4ace3faaacd8a3fd2f5c
LucasVasconcelosRibs <casluribeirovasconcelos@gmail.com>
2024-08-08 11:34:00 -0300

    new

---
+++ Aula12/ex012.js
@@ -0,0 +1,13 @@
+var Agora = new Date()
+var hora = Agora.getHours()
+
+console.log (`Agora sao ${hora} horas.`)
+if (hora < 12){
+    console.log('Bom dia')
+}
+else if (hora <= 18){
+    console.log('Boa tarde')
+}
+else {
+    console.log('Boa noite')
+}
