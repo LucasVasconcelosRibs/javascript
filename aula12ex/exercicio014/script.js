f1cd8f2580eaf1491c3dc82f6d5401d0197e4507
LucasVasconcelosRibs <casluribeirovasconcelos@gmail.com>
2024-08-14 11:35:49 -0300

    new

--- aula12ex/exercicio014/script.js
+++ aula12ex/exercicio014/script.js
@@ -4,4 +4,13 @@ var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas`
+if(hora >= 0 && hora < 12) {
+    //Bom dia!
+    img.src = `manha.png`
+}
+else if (hora >= 12 && hora < 18) {//Boa tarde
+img.src = noite.png}
+else{//Boa noite!
+    img.src = `noite.png`
+}
 }
